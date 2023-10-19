4de4dedf0d40be15317df6b3a475250bdec88f5b
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-10-19 12:20:40 +0200

    mise en place des nouvelles pages

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from './pages/Home';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
