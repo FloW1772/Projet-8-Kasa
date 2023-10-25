4c92d316285c25cdeafc62cf3d8cdd3438385134
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-10-25 03:59:30 +0200

    réglage probleme route

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-// import { createRoot } from "react-dom/client";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
 import Home from './pages/Home';
-
+import FicheLogement from './pages/Fiche-Logement';
+// import APropos from './pages/a-propos';
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home/>} /> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/fiche-logement" element={<FicheLogement/>} />
+        <Route path="/a-propos" element={<APropos/>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
