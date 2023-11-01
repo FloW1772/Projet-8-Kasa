35c7b489ea56eb711e4c56290182e1b8adecde8a
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-01 01:42:03 +0100

    avancer

--- my-new-app/src/components/header/Header.jsx
+++ my-new-app/src/components/header/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './header.scss'
 import Logo from '../../assets/logo/LOGO.png'
-// import Section from '../../assets/images/_Section_1.png'
 
 export default function Header() {
   return (
-    <div>
+<div className="container">
+      <div className="logo">
       <img src={Logo} alt="logo-kasa" />
+      </div>
+      <div className="text">
+        <h1 className="premier">Accueil</h1>
+        <h1 className="deuxieme">A Propos</h1>
 
-      {/* <div>
-        <img src={Section} alt="Section-kasa" />
-      </div> */}
       <nav>
 
       </nav>
     </div>
+    </div>
+
   )
 }
--- my-new-app/src/pages/Home.jsx
+++ my-new-app/src/pages/Home.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
 import Card from '../components/card/Card'
+import Section from '../assets/images/_Section_1.png'
+import './home.scss'
+import Redcard from '../../assets/icones/card.jpg'
+
+
+
 
 export default function Home() {
   const [logements, setLogements] = useState([])
@@ -18,9 +24,13 @@ export default function Home() {
   return (
     <div>
       <Header/>
-      <h1>Accueil</h1>
+      <div>
+      <img src={Section} alt="Section-kasa" class={Section} />
+    </div>
+
         {logements?.map((logement)=>(
           <Card key={logement.id} title={logement.title} cover={logement.cover} />
+          
           ))}
       <Footer/>
     </div>
