6f4f86a36efa5c3089c9764c5ceeeb28379f3427
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-02 11:18:08 +0100

    mise a jour

--- my-new-app/src/pages/ErrorPage.jsx
+++ my-new-app/src/pages/ErrorPage.jsx
@@ -5,23 +5,21 @@ import Footer from '../components/footer/Footer'
 
 
 const PageNotFound = () => {
+
   const [error, setError] = React.useState("Oups! La page que vous demandez n'existe pas.");
 
   return (
-    <div>
+    <div className="container">
       <Header />
 
-      <div className="container">
-        <h1 className="title">404</h1>
-        <p className="text">{error}</p>
-        <a className="link" href="/">
+      <h1 className="title">404</h1>
+      <p className="text">{error}</p>
+      <a className="link" href="/">
           Retourner à la page d'accueil
         </a>
 
-        <Footer />
-        </div>
-
-
+      <Footer />
+    </div>
   );
 };
 
