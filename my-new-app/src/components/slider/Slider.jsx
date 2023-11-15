84c21604ffaf2554d2336bca6978054b86f709dc
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-15 17:21:40 +0100

    début mise en forme du slider

--- my-new-app/src/components/slider/Slider.jsx
+++ my-new-app/src/components/slider/Slider.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './slider.scss';
 
-export default function Slider({pictures}) {
+const Slider = ({ pictures }) => {
+  const [currentImage, setCurrentImage] = useState(pictures[0]); // Manage the currently displayed image
+
+  const handleNextSlide = () => {
+    if (pictures.length > 1) {
+      const nextImageIndex = (pictures.indexOf(currentImage) + 1) % pictures.length;
+      setCurrentImage(pictures[nextImageIndex]);
+    }
+  };
+
   return (
-    <div>{pictures[0]}</div>
-  )
-}
+    <div className="slider-container">
+      <div className="slider-images">
+        {pictures.map((picture, index) => {
+          const isCurrentImage = picture === currentImage; // Check if the image is currently displayed
+          return (
+            <div key={index} className={`slider-image ${isCurrentImage ? 'active' : 'hidden'}`} style={{ visibility: isCurrentImage ? 'visible' : 'hidden' }}>
+              <img src={picture} alt="" />
+            </div>
+          );
+        })}
+      </div>
+
+      {/* Navigation button */}
+      <button className="slider-button next" onClick={handleNextSlide}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Slider;
