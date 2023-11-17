4238b011317b3138dcfc62f501e80e17e6edde26
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-17 11:51:22 +0100

    début mise en place rating

--- my-new-app/src/components/rating/Rating.jsx
+++ my-new-app/src/components/rating/Rating.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-export default function Rating({ratings}) {
+export default function Rating({score}) {
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    if (score >= i + 1) {
+      stars.push(<i className="fa fa-star" />);
+    } else {
+      stars.push(<i className="fa fa-star-o" />);
+    }
+  }
+
   return (
-    <div>{ratings}</div>
+    <div>{stars}</div>
   )
 }
