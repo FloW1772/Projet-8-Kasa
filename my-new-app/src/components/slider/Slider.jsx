591528d3042ed980e84daf7a0693849c69854f25
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-21 13:17:34 +0100

    tentative mise en forme slider

--- my-new-app/src/components/slider/Slider.jsx
+++ my-new-app/src/components/slider/Slider.jsx
@@ -1,57 +1,22 @@
-import React, { useState } from 'react';
+import React from "react";
 import './slider.scss';
+import Slider from "react-slick";
 
-
-const Slider = ({ pictures }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(1);
-
-  const handleNextSlide = () => {
-    if (currentImageIndex < pictures.length) {
-      setCurrentImageIndex(currentImageIndex + 1);
-    } else {
-      setCurrentImageIndex(0);
-    }
-  };
-
-  const handlePreviousSlide = () => {
-    if (currentImageIndex > 0) {
-      setCurrentImageIndex(currentImageIndex - 1);
-    } else {
-      setCurrentImageIndex(pictures.length - 1);
-    }
-  };
-
+const SliderCollapse = ({ pictures }) => {
   return (
-    <div className="slider-container">
-      <div className="slider-images">
-        {pictures.map((picture, index) => {
-          const isCurrentImage = index === currentImageIndex;
-          return (
-            <div key={index} style={{
-              position: 'absolute',
-              top: '-9999px',
-              left: '-9999px',
-              width: '100%',
-              height: '100%',
-              zIndex: isCurrentImage ? 1 : 0,
-            }}>
-              <img src={picture} alt="" />
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="slider-navigation">
-        <button className="slider-button prev" onClick={handlePreviousSlide}>
-          <i class="fa-solid fa-chevron-left"></i>
-        </button>
-
-        <button className="slider-button next" onClick={handleNextSlide}>
-          <i class="fa-solid fa-chevron-right"></i>
-        </button>
-      </div>
+    <div className="cover">
+      {pictures && (
+        <Slider
+          pictures={pictures}
+          slidesToShow={1}
+          slidesToScroll={1}
+          arrows={false}
+          dots={false}
+          autoplay={false}
+        />
+      )}
     </div>
   );
 };
 
-export default Slider;
+export default SliderCollapse;
