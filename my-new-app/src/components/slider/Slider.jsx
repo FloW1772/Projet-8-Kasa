a5a650077ab82376bf2e8e725da4752308e8546e
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-22 17:29:43 +0100

    mise en place des icones

--- my-new-app/src/components/slider/Slider.jsx
+++ my-new-app/src/components/slider/Slider.jsx
@@ -1,36 +1,38 @@
-import {useState} from "react";
+import { useState } from "react";
 import './slider.scss';
 
 
 const Slider = ({ pictures }) => {
-  const [currentSlide, setCurrentSlide]=useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0)
   console.log(pictures)
 
-const goToNextSlide =() => {
-setCurrentSlide((nextSlide)=>nextSlide === pictures.length-1 ? 0 : nextSlide+1)
-}
+  const goToNextSlide = () => {
+    setCurrentSlide((nextSlide) => nextSlide === pictures.length - 1 ? 0 : nextSlide + 1)
+  }
 
-const goToPrevSlide =() => {
-  setCurrentSlide((prevSlide)=>prevSlide === 0 ? pictures.length-1 : prevSlide-1)
+  const goToPrevSlide = () => {
+    setCurrentSlide((prevSlide) => prevSlide === 0 ? pictures.length - 1 : prevSlide - 1)
 
-}
+  }
 
 
   return (
     <div className="slider-container">
       <img src={pictures[currentSlide]} alt="" />
-      <div className="slider-btn-prev" onClick={goToPrevSlide}>
-précédent
+      <div class="slider-btn-prev" onClick={goToPrevSlide}>
+        <i class="fa-solid fa-chevron-left"></i>
       </div>
+
 
       <div className="slider-btn-next" onClick={goToNextSlide}>
-suivant
+        <i class="fa-solid fa-chevron-right"></i>
       </div>
 
+
       <div>
-        {currentSlide+1}/{pictures.length}
+        {currentSlide + 1}/{pictures.length}
       </div>
-  
+
     </div>
   );
 };
