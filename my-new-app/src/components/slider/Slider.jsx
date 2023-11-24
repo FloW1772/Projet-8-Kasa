414971dfabbdb278d93613d5ad9ce15b7efc41e7
FloW1772 <58610562+FloW1772@users.noreply.github.com>
2023-11-24 12:33:34 +0100

    mise a jour icone

--- my-new-app/src/components/slider/Slider.jsx
+++ my-new-app/src/components/slider/Slider.jsx
@@ -1,42 +1,40 @@
 import { useState } from "react";
-import './slider.scss';
-import left from '../../assets/icones/left.png'
-import right from '../../assets/icones/right.png'
+import "./slider.scss";
+import leftImage from "../../assets/icones/left.png";
+import rightImage from "../../assets/icones/right.png";
 
 const Slider = ({ pictures }) => {
-  const [currentSlide, setCurrentSlide] = useState(0)
-  console.log(pictures)
+  const [currentSlide, setCurrentSlide] = useState(0);
+  console.log(pictures);
 
   const goToNextSlide = () => {
-    setCurrentSlide((nextSlide) => nextSlide === pictures.length - 1 ? 0 : nextSlide + 1)
-  }
+    setCurrentSlide(
+      (nextSlide) => (nextSlide === pictures.length - 1 ? 0 : nextSlide + 1)
+    );
+  };
 
   const goToPrevSlide = () => {
-    setCurrentSlide((prevSlide) => prevSlide === 0 ? pictures.length - 1 : prevSlide - 1)
-
-  }
-
+    setCurrentSlide(
+      (prevSlide) => (prevSlide === 0 ? pictures.length - 1 : prevSlide - 1)
+    );
+  };
 
   return (
     <div className="slider-container">
       <img src={pictures[currentSlide]} alt="" />
-      <div class="slider-btn-prev" onClick={goToPrevSlide}>
-        <img src="left" />
+      <div className="slider-btn-prev" onClick={goToPrevSlide}>
+        <img src={leftImage} alt="Previous" />
       </div>
-
 
       <div className="slider-btn-next" onClick={goToNextSlide}>
-        <img src="right"/>
+        <img src={rightImage} alt="Next" />
       </div>
-
 
       <div>
         {currentSlide + 1}/{pictures.length}
       </div>
-
     </div>
   );
 };
-
 
 export default Slider;
